d7bf7efc7bf82210d7ba906d9814f888003ab7ad
19alexandr80 <usanka1980@gmail.com>
2023-02-27 18:48:06 +0200

    hw#8/2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
-
+import Vimeo from '@vimeo/player';
+console.log(Vimeo);
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo(iframe);
+console.log(localStorage.getItem('videoplayer-current-time'));
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+player.on('timeupdate', e => {
+  //   console.log(e.seconds);
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+});
