8558ef478ebc2b094ac32eff5edac2f9d1fc3ac9
19alexandr80 <usanka1980@gmail.com>
2023-02-28 19:00:41 +0200

    hw#8/3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,13 @@
+const throttle = require('lodash.throttle');
 import Vimeo from '@vimeo/player';
-console.log(Vimeo);
 const iframe = document.getElementById('vimeo-player');
 const player = new Vimeo(iframe);
-console.log(localStorage.getItem('videoplayer-current-time'));
 if (localStorage.getItem('videoplayer-current-time')) {
   player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 }
-player.on('timeupdate', e => {
-  //   console.log(e.seconds);
+const throttieOnTimeupdateLocal = throttle(onTimeupdateLocal, 1000);
+function onTimeupdateLocal(e) {
   localStorage.setItem('videoplayer-current-time', e.seconds);
-});
+  console.log(e.seconds);
+}
+player.on('timeupdate', throttieOnTimeupdateLocal);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
-
+const throttle = require('lodash.throttle');
+const formElem = document.querySelector('.feedback-form');
+if (localStorage.getItem('message')) {
+  formElem.elements.message.value = localStorage.getItem('message');
+}
+if (localStorage.getItem('email')) {
+  formElem.elements.email.value = localStorage.getItem('email');
+}
+const throttieOnformDa = throttle(formDa, 500);
+function formDa(e) {
+  const message = e.currentTarget.elements.message.value;
+  const email = e.currentTarget.elements.email.value;
+  localStorage.setItem('message', message);
+  localStorage.setItem('email', email);
+  console.log(message);
+  console.log(email);
+}
+formElem.addEventListener('input', throttieOnformDa);
+formElem.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+  e.preventDefault();
+  localStorage.removeItem('message');
+  localStorage.removeItem('email');
+  const consoleLog = {};
+  const formData = new FormData(e.currentTarget);
+  formData.forEach((a, b) => {
+    consoleLog[b] = a;
+  });
+  console.log(consoleLog);
+  formElem.elements.email.value = '';
+  formElem.elements.message.value = '';
+}
