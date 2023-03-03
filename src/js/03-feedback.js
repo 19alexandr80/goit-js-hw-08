c6b62745c0eea7fd9aab8aae1e0e44bec727e4d1
19alexandr80 <usanka1980@gmail.com>
2023-03-03 03:26:21 +0200

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,14 @@
 const throttle = require('lodash.throttle');
 const formElem = document.querySelector('.feedback-form');
+const objFormLocal = {};
 if (localStorage.getItem('feedback-form-state')) {
   const saveForm = JSON.parse(localStorage.getItem('feedback-form-state'));
   for (const key in saveForm) {
+    objFormLocal[key] = saveForm[key];
     formElem.elements[key].value = saveForm[key];
   }
 }
-const objFormLocal = {};
+// const objFormLocal = {};
 function onFormSaveLocal(e) {
   objFormLocal[e.target.name] = e.target.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(objFormLocal));
