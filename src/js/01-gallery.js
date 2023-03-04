a89d60946226854e28d61b86053e64b220ee98da
19alexandr80 <usanka1980@gmail.com>
2023-03-04 18:28:49 +0200

    handlebars-precompile

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,27 +1,12 @@
 // Add imports above this line
+import sampleCards from './galleryCards.hbs';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
 console.log(galleryItems);
 const galleryEl = document.querySelector('.gallery');
-const createGallery = galleryItems
-  .map(({ preview, original, description }) => {
-    return `<div class="gallery__item">
-            <a class="gallery__link" href="${original}">
-                <img
-                    class="gallery__image"
-                    src="${preview}"
-                    data-source="${original}"
-                    alt="${description}"
-                />
-            </a>
-        </div>`;
-  })
-  .join('');
-galleryEl.insertAdjacentHTML('beforeEnd', createGallery);
-
+galleryEl.insertAdjacentHTML('beforeEnd', sampleCards(galleryItems));
 const lightbox = new SimpleLightbox('.gallery a', {
   /* options */
   captionsData: 'alt',
